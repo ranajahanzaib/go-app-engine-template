d378546282114665943c541595d2bcd5c2423009
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-19 19:18:07 +0500

    .

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"google.golang.org/appengine"
+)
+
+func main() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/about", about)
+	appengine.Main()
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "A homepage.")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "A about page.")
+}
